3a2e20fe4be89202b3c4b728e0e228551b7f203f
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-01-29 08:50:46 +0100

    example of the module patern(used befor ES6)

--- script.js
+++ script.js
@@ -37,17 +37,50 @@ console.log(cart);
 // console.log('Something');
 
 // example2:
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
-  console.log(data);
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   console.log(data);
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
-// const lastPost = getLastPost();
-// not wery clean
-// lastPost.then(last => console.log(last));
-// console.log(lastPost);
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+// // const lastPost = getLastPost();
+// // not wery clean
+// // lastPost.then(last => console.log(last));
+// // console.log(lastPost);
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+///////////////////////////////////////////////////////////////
+// Module patern - to w jaki sposób były tworzone module, zanim pojawiły się prawdzie module in ES6
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from suplier`);
+  };
+
+  // zwracamy to co potrzebne jest globalnie wzakresie modula
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})(); //closures - funkcja zistałą wywołąna, ale wciąż mamy dostęp do zmiennych, utworzonych w jej środku, które ta funkcja zwróciła
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); //undefined - ponieważ jest w zakresie prywatnym
